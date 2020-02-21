6a8f460c2e1926ed55213a32c17e19ef6e9824d6
DESKTOP-EQBP3A1\hebin <hebin20004@126.com>
2020-02-22 00:02:47 +0800

    1st check in

---
+++ mysite/polls/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
+
+
+
